207c172fc2e621ac8f33e0844ba4ca6aa79e0812
Philippe Nénert <philippe.nenert@arkena.com>
2014-07-16 17:31:00 +0200

    Add the first test ;-)

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -1,15 +1,32 @@
-require '~/blog/spec/rails_helper.rb'
-require '~/blog/app/controllers/articles_controller.rb'
+require 'rails_helper.rb'
+# require '~/blog/app/controllers/articles_controller.rb'
 
-describe ArticlesController do
+describe 'ArticlesController' do
 	describe '#create' do
-		it 'should NOT save article with title with less than 5 character' do
-			subject {post :create, :article => {:title => ''}}
-			expect(subject).not_to redirect_to(@article)
+		# avant chaque test, il faut ...
+    before(:each) do
+    	# autentification
+    end
+
+		it 'should not save a article without a title' do
+			# nouvelle article
+			article = Article.new
+			article.save
+			expect article.errors.count > 0
+			# subject {post :create, :article => {:title => ''}}
+			# expect(subject).not_to redirect_to(@article)
 		end
+
+		it 'should NOT save article if title length less than 5 character' do
+			article = Article.new
+			article.title = '1234'
+			article.save
+			expect article.errors.count > 0
+		end
+
 		it 'should save article with title with more than 5 character' do
 			subject {post :create, :article => {:title => '012345'}}
-			expect(subject).to redirect_to(@article)
+			expect(subject).to redirect_to(article)
 		end
 	end
 end
